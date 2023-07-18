947ad643a29e99ca2883b9e670af23bb867c28e8
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-17 20:26:39 -0400

    initial Hero and Projects components

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Hero from '@/components/Hero'
+import Projects from '@/components/Projects'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Hero />
+      <Projects/>
     </main>
   )
 }
